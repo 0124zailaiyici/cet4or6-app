d51615505307167f2477a9d2034eaeb33428d981
0124zailaiyici <3249875603@qq.com>
2026-05-26 13:40:59 +0800

    fix: 三个真机bug修复

--- miniprogram/pages/exam/exam-listening/exam-listening.ts
+++ miniprogram/pages/exam/exam-listening/exam-listening.ts
@@ -1,7 +1,7 @@
 import listeningData from '../../../data/listening'
 import { applyTheme, getDarkMode } from '../../../utils/theme'
 
-const API_BASE = 'http://localhost:3001'
+const API_BASE = (() => { try { return wx.getStorageSync('api_base') || 'https://cet4or6-app-production.up.railway.app' } catch(_) { return 'https://cet4or6-app-production.up.railway.app' } })()
 
 interface IListeningQ {
   qi: number
--- miniprogram/pages/listening/listening.ts
+++ miniprogram/pages/listening/listening.ts
@@ -2,7 +2,7 @@ import listeningData from '../../data/listening_generated'
 import { doCheckIn } from '../../utils/checkin'
 import { applyTheme, getDarkMode } from '../../utils/theme'
 
-const API_BASE = 'http://localhost:3001'
+const API_BASE = (() => { try { return wx.getStorageSync('api_base') || 'https://cet4or6-app-production.up.railway.app' } catch(_) { return 'https://cet4or6-app-production.up.railway.app' } })()
 
 interface ISentence {
   text: string
